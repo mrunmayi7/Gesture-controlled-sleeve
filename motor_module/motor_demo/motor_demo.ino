5c8c4d43b694e7196fea9a1f35deccff88918df1
swanganie <swang166@illinois.edu>
2018-04-09 21:00:03 -0500

    updated motor_demo basic

--- motor_module/motor_demo/motor_demo.ino
+++ motor_module/motor_demo/motor_demo.ino
@@ -4,11 +4,16 @@
  * Bluetooth not yet integrated
  */
 
-const int motorPin = 3; // ouput pin for vibration motor
+// --------- CONSTANTS ---------
+// Assign pin numbers
+const int motorPin = 3; // output pin for vibration motor
 const int pushPin = 2;  // input pin for push button
-int pushVal = 0;  // variable for reading the pin status
+
+// --------- VARIABLES ---------
+int pushVal = LOW;
 int motorFlag = 0;
-int i = 0;
+int motorCount = 0;
+int motorOn = 51*2.0;
 
 void setup() {
   pinMode(motorPin, OUTPUT);
@@ -21,7 +26,18 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("\nRESTART");
+  delay(5000);
+  
   pushVal = digitalRead(pushPin); // read input value from push button
+  Serial.print("Current pushVal = ");
+  delay(1000);
+  Serial.println(pushVal);
+  
+  if (pushVal == HIGH)
+    Serial.println("Button was pushed");
+  else
+    Serial.println("Button was not pushed");
 
   // Checks request to activate motor and marks flag
   // if pushVal input is HIGH (button released)
@@ -32,23 +48,25 @@ void loop() {
 
   if (motorFlag == 1) {
     // Vibrate motor 5 times and interrupt to turn off/mute if button is pressed again
-    i = 0;
+    motorCount = 0;
     do {
-      digitalWrite(motorPin, HIGH);
+      analogWrite(motorPin, motorOn);
       delay(500);
       
-      digitalWrite(motorPin, LOW);
+      analogWrite(motorPin, 0);
       delay(100);
-      i++;
-    } while ((i < 5) && (pushVal != HIGH));
+      motorCount++;
+      Serial.print(motorCount);
+    } while (motorCount < 5);
+//    } while ((motorCount < 5) && (pushVal != HIGH));
 
     // Turn off motor until next activation and set flag back to 0
-    digitalWrite(motorPin, LOW);
+    analogWrite(motorPin, 0);
     
     motorFlag = 0;
   }
   else if (motorFlag == 0) {
-    digitalWrite(motorPin, LOW);
+    analogWrite(motorPin, 0);
   }
 
 }
